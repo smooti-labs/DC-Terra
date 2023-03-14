113072774ad573a96e4301487d439d41076e198e
smooti <kobeowens43099@gmail.com>
2023-03-13 21:08:08 -0400

    Adding windows 10 vm to join domain.

--- main.tf
+++ main.tf
@@ -14,8 +14,13 @@ data "vsphere_host" "host" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "vsphere_virtual_machine" "template" {
+data "vsphere_virtual_machine" "template_dc" {
   name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template_windows_ws" {
+  name          = "Windows-10-22H2-031323"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
@@ -24,7 +29,7 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-# System to deploy
+# Deploy and provision domain controller
 resource "vsphere_virtual_machine" "domain_controller" {
   count                   = var.vm_count
   name                    = "${var.vm_name}-${count.index}"
@@ -44,7 +49,7 @@ resource "vsphere_virtual_machine" "domain_controller" {
     size  = var.vm_disk_size
   }
   clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
+    template_uuid = data.vsphere_virtual_machine.template_dc.id
     customize {
       timeout = 30
       windows_options {
@@ -62,4 +67,44 @@ resource "vsphere_virtual_machine" "domain_controller" {
   provisioner "local-exec" {
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${ var.vm_ipv4_address },' ansible/windows_dc/main.yaml"
   }
+}
+
+# Windows client
+resource "vsphere_virtual_machine" "windows_ws" {
+  depends_on = [vsphere_virtual_machine.domain_controller]
+
+  count                   = 2
+  name                    = "win-${count.index}"
+  num_cpus                = 2
+  num_cores_per_socket    = 2
+  memory                  = 4112
+  resource_pool_id        = data.vsphere_host.host.resource_pool_id
+  guest_id                = "windows9_64Guest"
+  scsi_type               = var.vm_scsi_type
+  firmware                = "efi"
+  efi_secure_boot_enabled = true
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = var.vm_disk_size
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template_windows_ws.id
+    customize {
+      timeout = 30
+      windows_options {
+        computer_name = "win-${count.index}"
+		join_domain = "blah.local"
+		domain_admin_user = "solaire"
+		domain_admin_password = "1qaz2wsx!QAZ@WSX"
+      }
+      network_interface {
+
+      }
+      ipv4_gateway = var.vm_ipv4_gateway
+    }
+  }
+  folder = var.folder
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
-vsphere_server     = "vcu.rtapu.local"
-vsphere_datacenter = "VxRail-Datacenter"
-vsphere_host       = "vsx-01.rtapu.local"
+vsphere_server     = "blah.local"
+vsphere_datacenter = "blah-Datacenter"
+vsphere_host       = "blah.local"
 
+# Domain Controller Configuration
 template_name = "Windows-Server-2022-Datacenter-030823"
-network_name  = "NSX-VLAN-200"
-
+network_name  = "VLAN-20"
 vm_name      = "DomainCon"
 vm_count     = 1
 vm_cpus      = 2
@@ -13,9 +13,8 @@ vm_memory    = 4112
 vm_guest_id  = "windows2019srv_64Guest"
 vm_disk_size = 60
 vm_scsi_type = "lsilogic-sas"
-
-vm_ipv4_address = "172.16.5.15"
+vm_ipv4_address = ""
 vm_ipv4_netmask = 24
-vm_ipv4_gateway = "172.16.5.1"
+vm_ipv4_gateway = ""
 
-folder = "Testing"
+folder = "Research"
