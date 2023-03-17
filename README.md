2186c241f3d0d9dcf72c861ac4bf8ffbd758103e
smooti <kobeowens43099@gmail.com>
2023-03-17 17:40:09 -0400

    fmt

--- README.md
+++ README.md
@@ -3,30 +3,13 @@
 ```powershell
 $env:VSPHERE_USER = 'MyUserName'
 $env:VSPHERE_PASSWORD = 'MyPassword'
+$env:VSPHERE_SERVER = 'myvcenter.local'
+$env:VSPHERE_ALLOW_UNVERIFIED_SSL='true' # OPTIONAL
 ```
 
 ```bash
 export VSPHERE_USER='MyUserName'
 export VSPHERE_PASSWORD='MyPassword'
-```
-## Required Variables
-You are able to create a `terraform.tfvars` file containing these required variables.
-
-```terraform
-# vSphere information
-vcenter_server     = "vcenter1v.lordran.local"
-vsphere_datacenter = "Datacenter"
-vsphere_host       = "esxi1p.lordran.local"
-
-# Domain Controller Configuration
-template        = "Windows-Server-2022-Datacenter-031723"
-network         = "Research"
-vm_name         = "DomainCon"
-vm_guest_id     = "windows2019srv_64Guest"
-vm_ipv4_address = "172.16.5.15"
-vm_ipv4_netmask = 24
-vm_ipv4_gateway = "172.16.5.1"
-
-dns_domain_name = "rootkit.local"
-folder          = "Research"
+export VSPHERE_SERVER='myvcenter.local'
+export VSPHERE_ALLOW_UNVERIFIED_SSL='true' # OPTIONAL
 ```
--- main.tf
+++ main.tf
@@ -1,69 +1,15 @@
-provider "vsphere" {
-  vsphere_server       = var.vcenter_server
-  allow_unverified_ssl = true
-}
-
-data "vsphere_datacenter" "datacenter" {
-  name = var.vsphere_datacenter
-}
-
-data "vsphere_host" "host" {
-  name          = var.vsphere_host
-  datacenter_id = data.vsphere_datacenter.datacenter.id
-}
-
-data "vsphere_virtual_machine" "template_dc" {
-  name          = var.template
-  datacenter_id = data.vsphere_datacenter.datacenter.id
-}
-
-data "vsphere_network" "network" {
-  name          = var.network
-  datacenter_id = data.vsphere_datacenter.datacenter.id
-}
-
-locals {
-  vm_scsi_type = "lsilogic-sas"
-  firmware     = "efi"
-  secure_boot  = true
-  vm_timeout   = 30
-}
-
-# Deploy and provision domain controller
-resource "vsphere_virtual_machine" "domain_controller" {
-  name                    = var.vm_name
-  num_cpus                = var.vm_cpus
-  num_cores_per_socket    = var.vm_cores
-  memory                  = var.vm_memory
-  resource_pool_id        = data.vsphere_host.host.resource_pool_id
-  guest_id                = var.vm_guest_id
-  scsi_type               = local.vm_scsi_type
-  firmware                = local.firmware
-  efi_secure_boot_enabled = local.secure_boot
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-  disk {
-    label = "disk0"
-    size  = var.vm_disk_size
-  }
-  clone {
-    template_uuid = data.vsphere_virtual_machine.template_dc.id
-    customize {
-      timeout = local.vm_timeout
-      windows_options {
-        computer_name = var.vm_name
-      }
-      network_interface {
-        ipv4_address = var.vm_ipv4_address
-        ipv4_netmask = var.vm_ipv4_netmask
-      }
-      ipv4_gateway = var.vm_ipv4_gateway
-    }
-  }
-  folder = var.folder
-
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${var.vm_ipv4_address},' ansible/windows_dc/main.yaml --extra-vars 'ansible_user=solaire ansible_password=1qaz2wsx!QAZ@WSX safe_mode_password=${var.dsrm_password} dns_domain_name=${var.dns_domain_name} domain_admin_password=1qaz2wsx!QAZ@WSX'"
-  }
+module "domain-controller" {
+	source = "./modules/deploy-domain-controller"
+	vsphere_datacenter = "Datacenter"
+	vsphere_host       = "esxi1p.lordran.local"
+	template        = "Windows-Server-2022-Datacenter-031723"
+	network         = "Research"
+	name         = "DomainCon"
+	guest_id     = "windows2019srv_64Guest"
+	ipv4_address = "172.16.5.15"
+	ipv4_netmask = 24
+	ipv4_gateway = "172.16.5.1"
+	dns_domain_name = "rootkit.local"
+	folder          = "Research"
+	dsrm_password = "1qaz2wsx!QAZ@WSX"
 }
