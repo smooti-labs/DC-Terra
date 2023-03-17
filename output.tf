268ed85ab737a61304cf568a686e9962cb4e5f56
smooti <kobeowens43099@gmail.com>
2023-03-17 18:59:07 -0400

    fmt

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "computer_name" {
-	value = module.domain-controller.computer_name
+  value = module.domain-controller.computer_name
 }
 
 output "ipv4_address" {
-	value = module.domain-controller.ipv4_address
+  value = module.domain-controller.ipv4_address
 }
