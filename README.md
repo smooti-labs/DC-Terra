feb1803330effcffc92d5c70adc663eb74397f10
smooti <kobeowens43099@gmail.com>
2023-03-17 17:44:16 -0400

    hiding var

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 $env:VSPHERE_USER = 'MyUserName'
 $env:VSPHERE_PASSWORD = 'MyPassword'
 $env:VSPHERE_SERVER = 'myvcenter.local'
+$env:TF_VAR_dsrm_password = 'myDSRMpassword'
 $env:VSPHERE_ALLOW_UNVERIFIED_SSL='true' # OPTIONAL
 ```
 
@@ -11,5 +12,6 @@ $env:VSPHERE_ALLOW_UNVERIFIED_SSL='true' # OPTIONAL
 export VSPHERE_USER='MyUserName'
 export VSPHERE_PASSWORD='MyPassword'
 export VSPHERE_SERVER='myvcenter.local'
+export TF_VAR_dsrm_password='myDSRMpassword'
 export VSPHERE_ALLOW_UNVERIFIED_SSL='true' # OPTIONAL
 ```
--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 module "domain-controller" {
-	source = "./modules/deploy-domain-controller"
-	vsphere_datacenter = "Datacenter"
-	vsphere_host       = "esxi1p.lordran.local"
-	template        = "Windows-Server-2022-Datacenter-031723"
-	network         = "Research"
-	name         = "DomainCon"
-	guest_id     = "windows2019srv_64Guest"
-	ipv4_address = "172.16.5.15"
-	ipv4_netmask = 24
-	ipv4_gateway = "172.16.5.1"
-	dns_domain_name = "rootkit.local"
-	folder          = "Research"
-	dsrm_password = "1qaz2wsx!QAZ@WSX"
+  source             = "./modules/deploy-domain-controller"
+  vsphere_datacenter = "Datacenter"
+  vsphere_host       = "esxi1p.lordran.local"
+  template           = "Windows-Server-2022-Datacenter-031723"
+  network            = "Research"
+  name               = "DomainCon"
+  guest_id           = "windows2019srv_64Guest"
+  ipv4_address       = "172.16.5.15"
+  ipv4_netmask       = 24
+  ipv4_gateway       = "172.16.5.1"
+  dns_domain_name    = "rootkit.local"
+  folder             = "Research"
 }
